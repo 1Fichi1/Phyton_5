eebd2cbc5001d62819b5f5d890ba09f1ac9af3c3
1Fichi1 <144726159+1Fichi1@users.noreply.github.com>
2023-12-26 22:16:02 +0300

    main.py

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
+# Импорт библиотек Pandas и NumPy
 import pandas as pd
 import numpy as np
 
-# Загрузка датасета
+# Загрузка данных из файла 'Mall_Customers.csv' в DataFrame
 Mall_Customers = pd.read_csv('Mall_Customers.csv')
 Mall_Customers
 
-# вывод
+# Вывод первых нескольких строк (5) из DataFrame
 Mall_Customers.head()
 
-# размерность массива
+# Вывод размерности (количество строк и столбцов) DataFrame
 Mall_Customers.shape
 
-# вывод намименований всех колонок
+# Вывод списка названий всех столбцов DataFrame
 Mall_Customers.columns
 
-# Вывод всех уникальных значений
+# Вывод уникальных значений из столбца 'Spending Score (1-100)'
 Mall_Customers['Spending Score (1-100)'].unique()
 
-# Отсортировка по определенным параметрам
+# Сортировка данных по столбцу 'CustomerID'
 Mall_Customers.sort_values(by='CustomerID')
 
-# Удаление ненужных столбцов или строк
+# Удаление столбца 'Genre' из DataFrame
 Mall_Customers.drop(columns = ["Genre"], inplace=True)
 
-# Заменение определенных значений в датасете (например, пустые)
+# Замена всех уникальных значений в столбце 'Age' на "immortal"
 Mall_Customers["Age"] = Mall_Customers["Age"].replace(Mall_Customers["Age"].unique(), "immortal")
 Mall_Customers
 
-# Удаление дубликатов
+# Создание нового DataFrame Mall_Customersa, в котором удалены дубликаты
 Mall_Customersa = Mall_Customers.drop_duplicates()
 Mall_Customers
 
-# Проведение анализов с помощью функций info
+# Вывод общей информации о DataFrame
 Mall_Customers.info()
 
-# Проведение анализа с помощью функций describe
+# Вывод основных характеристик данных в DataFrame
 Mall_Customers.describe()
 
-# Проведение выборки данных по строкам и столбцам с помощью loc
+# Создание подмножества данных, выбирая строки 0, 1, 2 и столбцы 'Annual Income (k$)' и 'Spending Score (1-100)'
 subset = Mall_Customers.loc[[0, 1, 2], ['Annual Income (k$)', 'Spending Score (1-100)']]
 subset
 
 # Сохранение нового датасетаa
 Mall_Customers.to_csv("Mall_Customers_NEW.csv", index=True)
+# Вывод исходного DataFrame.
 Mall_Customers
 
 
